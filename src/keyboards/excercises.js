015ba5defbb09b36d7e77b5c0bb978b80e8b67bd
Babak <Babakdevgeek@gmail.com>
2025-12-26 22:35:26 +0330

    feat: Excercises keyboard file created

---
+++ src/keyboards/excercises.js
@@ -0,0 +1,8 @@
+import { Markup } from "telegraf";
+export const mainKeyboard = Markup.inlineKeyboard([
+    [Markup.button.callback("📋 حرکات من", "list_exercises")],
+    [Markup.button.callback("➕ اضافه کردن حرکت", "add_exercise")],
+]);
+export const backButton = Markup.inlineKeyboard([
+    [Markup.button.callback("⬅️ بازگشت", "back_home")],
+]);
