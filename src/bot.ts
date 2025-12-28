671cfeace93eda4043d19207712acd9d2ac9feee
Babak <Babakdevgeek@gmail.com>
2025-12-29 00:39:23 +0330

    fix: adjust all in one excercises in one message

--- src/bot.ts
+++ src/bot.ts
@@ -44,11 +44,13 @@ bot.hears(buttonsText.home.allExsInOneMessage, async (ctx) => {
   }
 
   // ساخت متن خوانا
-  let text = `📋 *لیست حرکات شما*\n━━━━━━━━━━━━━━\n`;
+  const line = "━━━━━━━━━━━━━━━━━━━━━━━━";
+
+  let text = `📋 *لیست حرکات شما*\n${line}\n`;
   user.exercises.forEach((ex) => {
     text += `🏋️ ${ex.name} | ست: ${ex.sets} | وزنه: ${ex.weight}kg\n`;
   });
-  text += "━━━━━━━━━━━━━━";
+  text += line;
 
   await ctx.replyWithPhoto(
     "https://www.primalstrength.com/cdn/shop/files/gymdesign_render_Two_collumn_grid_cb1b5850-fa8e-4a7b-a2b3-190c2e45facd.jpg?v=1680719688&width=500",
