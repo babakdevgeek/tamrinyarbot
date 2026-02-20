624625bebeff8d9b858b6a2686b32bbff887eaae
Babak <Babakdevgeek@gmail.com>
2026-02-20 12:07:43 +0330

    fix: excercices list now has back button on top and it works

--- src/keyboards/allExcercises.ts
+++ src/keyboards/allExcercises.ts
@@ -5,12 +5,7 @@ export async function getExercisesKeyboard(userId: number) {
   const exercises = await prisma.exercise.findMany({ where: { userId } });
 
   if (exercises.length === 0) return null;
-
-  // هر دکمه یک ردیف، شامل اسم حرکت
-  const buttons = exercises.map((ex) => [ex.name]);
-
-  // اضافه کردن دکمه بازگشت
-  buttons.push(["⬅️ بازگشت"]);
+  const buttons = [["⬅️"], ...exercises.map((ex) => [ex.name])];
 
   return Markup.keyboard(buttons).resize();
 }
