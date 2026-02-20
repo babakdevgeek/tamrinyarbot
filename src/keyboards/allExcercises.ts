24134c6fdc17c9142423f716afb89c3fd8e1d662
Babak <Babakdevgeek@gmail.com>
2026-02-20 12:13:56 +0330

    fix: changing order of excercies ro desc

--- src/keyboards/allExcercises.ts
+++ src/keyboards/allExcercises.ts
@@ -2,7 +2,10 @@ import { Markup } from "telegraf";
 import prisma from "../db.js";
 
 export async function getExercisesKeyboard(userId: number) {
-  const exercises = await prisma.exercise.findMany({ where: { userId } });
+  const exercises = await prisma.exercise.findMany({
+    where: { userId },
+    orderBy: { id: "desc" },
+  });
 
   if (exercises.length === 0) return null;
   const buttons = [["⬅️"], ...exercises.map((ex) => [ex.name])];
